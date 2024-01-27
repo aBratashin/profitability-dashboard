9b68b5a9c30ab98701a7ab4c0f62ad2c620ec5dd
Арсений Б. <ars.bratashin@gmail.com>
2024-01-27 18:20:09 +0300

    Add Component 1

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default function Auth() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <Progress type='grade' color='white' initialData={24} finalData={72}/>
+                <Progress type='grade' color='white' initialData={36} finalData={72}/>
             </div>
         </main>
     );
