e6f30f93d80c143c659cb932846224edb96098ec
GgWp897 <64257150+GgWp897@users.noreply.github.com>
2024-01-28 13:46:33 +0300

    Update Header.tsx

--- src/app/dashboard/components/Header.tsx
+++ src/app/dashboard/components/Header.tsx
@@ -30,14 +30,13 @@ const Header = () => {
                             className="rounded-full"
                         />
                     </div>
-                    <div
-                        className="pl-2 pr-3 py-2 rounded-[32px] border-2 border-neutral-200 justify-start items-center gap-2.5 flex">
-                        <div
-                            className="w-[37px] px-1.5 pb-0 bg-zinc-950 bg-opacity-50 rounded-[500px] justify-center items-center gap-2.5 flex">
-                            <div className="text-white text-sm font-['Suisse Int'l'] leading-[24px]">все</div>
+                    <div className="px-3 py-2 rounded-[32px] border-2 border-neutral-200 justify-start items-center gap-2.5 inline-flex">
+                        <div className="justify-start items-center gap-[3px] flex">
+                        <div className="w-3 h-3 bg-violet-500 rounded-[3px]" />
+                        <div className="w-3 h-3 bg-fuchsia-400 rounded-[3px]" />
                         </div>
-                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px] mt-0.5">Тип проекта</div>
-                        <div className="w-6 h-6 relative"/>
+                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px]">Тип проекта</div>
+                        <div className="w-6 h-6 relative" />
                         <Image
                             src="/img/arrow-down.svg"
                             width={24}
@@ -46,6 +45,8 @@ const Header = () => {
                             className="rounded-full"
                         />
                     </div>
+                    
+
                     <div
                         className="pl-2 pr-3 py-2 rounded-[32px] border-2 border-neutral-200 justify-start items-center gap-2.5 flex">
                         <div
