420c1cbd3fbe152e4c018597d06960ae31316860
GgWp897 <64257150+GgWp897@users.noreply.github.com>
2024-01-28 13:34:20 +0300

    Update Header.tsx

--- src/app/dashboard/components/Header.tsx
+++ src/app/dashboard/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Header = () => {
     return (
-        <div>
+        <div className='px-4 pt-5'>
             <div className="w-[100%] h-14 justify-between items-center inline-flex">
                 <div className="text-neutral-700 text-5xl font-medium font-['Suisse Intl']">Рентабельность</div>
                 <div className="justify-start items-start gap-4 flex">
@@ -17,10 +17,10 @@ const Header = () => {
                     <div
                         className="pl-2 pr-3 py-2 rounded-[32px] border-2 border-neutral-200 justify-start items-center gap-2.5 flex">
                         <div
-                            className="w-[37px] px-1.5 pb-0.5 bg-zinc-950 bg-opacity-50 rounded-[500px] justify-center items-center gap-2.5 flex">
-                            <div className="text-white text-sm font-['Suisse Int'l'] leading-[18px]">все</div>
+                            className="w-[37px] px-1.5 pb-0 bg-zinc-950 bg-opacity-50 rounded-[500px] justify-center items-center gap-2.5 flex">
+                            <div className="text-white text-sm font-['Suisse Int'l'] leading-[24px] mb-0.6">все</div>
                         </div>
-                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px]">Менеджеры</div>
+                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px] mt-0.5">Менеджеры</div>
                         <div className="w-6 h-6 relative"/>
                         <Image
                             src="/img/arrow-down.svg"
@@ -33,10 +33,10 @@ const Header = () => {
                     <div
                         className="pl-2 pr-3 py-2 rounded-[32px] border-2 border-neutral-200 justify-start items-center gap-2.5 flex">
                         <div
-                            className="w-[37px] px-1.5 pb-0.5 bg-zinc-950 bg-opacity-50 rounded-[500px] justify-center items-center gap-2.5 flex">
-                            <div className="text-white text-sm font-['Suisse Int'l'] leading-[18px]">все</div>
+                            className="w-[37px] px-1.5 pb-0 bg-zinc-950 bg-opacity-50 rounded-[500px] justify-center items-center gap-2.5 flex">
+                            <div className="text-white text-sm font-['Suisse Int'l'] leading-[24px]">все</div>
                         </div>
-                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px]">Тип проекта</div>
+                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px] mt-0.5">Тип проекта</div>
                         <div className="w-6 h-6 relative"/>
                         <Image
                             src="/img/arrow-down.svg"
@@ -49,10 +49,10 @@ const Header = () => {
                     <div
                         className="pl-2 pr-3 py-2 rounded-[32px] border-2 border-neutral-200 justify-start items-center gap-2.5 flex">
                         <div
-                            className="w-[37px] px-1.5 pb-0.5 bg-zinc-950 bg-opacity-50 rounded-[500px] justify-center items-center gap-2.5 flex">
-                            <div className="text-white text-sm font-['Suisse Int'l'] leading-[18px]">все</div>
+                            className="w-[37px] px-1.5 pb-0 bg-zinc-950 bg-opacity-50 rounded-[500px] justify-center items-center gap-2.5 flex">
+                            <div className="text-white text-sm font-['Suisse Int'l'] leading-[24px]">все</div>
                         </div>
-                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px]">Дата оплаты</div>
+                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px] mt-0.5">Дата оплаты</div>
                         <div className="w-6 h-6 relative"/>
                         <Image
                             src="/img/arrow-down.svg"
@@ -63,7 +63,7 @@ const Header = () => {
                         />
                     </div>
                     <div className="w-[90px] px-3 py-2 bg-white rounded-[32px] justify-start items-center gap-2.5 flex">
-                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[18px]">Очистить</div>
+                        <div className="text-zinc-800 text-sm font-['Suisse Int'l'] leading-[24px]">Очистить</div>
                     </div>
                 </div>
             </div>
