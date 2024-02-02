ea3ab742ddbaf01b5647013b11f77df5f7afd394
Арсений Б. <ars.bratashin@gmail.com>
2024-02-02 17:29:15 +0300

    clear warnings, change .gitignore

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,49 +1,56 @@
-import {Metadata} from 'next';
-import Header from "@/app/dashboard/components/Header";
-import Line from "@/app/dashboard/components/Line";
-import Progress from "@/components/progress/Progress";
-import SortItem from "@/components/sortItem/SortItem";
-import StageProgress from "@/components/stageProgress/StageProgress";
-import Category from "@/components/category/Category";
-import InvoiceProgress from "@/components/invoiceProgress/InvoiceProgress";
-import StageInvoiceAlert from "@/components/stageInvoiceAlert/StageInvoiceAlert";
-import Tooltip from "@/components/tooltip/Tooltip";
-import DropdownItem from "@/components/dropdownItem/DropdownItem";
-import DropdownContent from "@/components/dropdownContent/DropdownContent";
-import SortDropDown from "@/components/sortDropdown/SortDropDown";
-import StageItem from "@/components/stageItem/StageItem";
-import CardInfoUp from "@/components/cardInfo/CardInfoUp";
-import CardInfoDown from "@/components/cardInfo/CardInfoDown";
-import React from "react";
+import { Metadata } from 'next'
+import Header from '@/app/dashboard/components/Header'
+import Line from '@/app/dashboard/components/Line'
+import Progress from '@/components/progress/Progress'
+import SortItem from '@/components/sortItem/SortItem'
+import StageProgress from '@/components/stageProgress/StageProgress'
+import Category from '@/components/category/Category'
+import InvoiceProgress from '@/components/invoiceProgress/InvoiceProgress'
+import StageInvoiceAlert from '@/components/stageInvoiceAlert/StageInvoiceAlert'
+import Tooltip from '@/components/tooltip/Tooltip'
+import DropdownItem from '@/components/dropdownItem/DropdownItem'
+import DropdownContent from '@/components/dropdownContent/DropdownContent'
+import SortDropDown from '@/components/sortDropdown/SortDropDown'
+import StageItem from '@/components/stageItem/StageItem'
+import CardInfoUp from '@/components/cardInfo/CardInfoUp'
+import CardInfoDown from '@/components/cardInfo/CardInfoDown'
+import React from 'react'
 
 export const metadata: Metadata = {
-    title: 'Dashboard',
-};
+	title: 'Dashboard'
+}
 
 export default function Auth() {
-    return (
-        <main className='flex flex-col items-center justify-center h-screen'>
-            <Header/>
-            <Line/>
-            <Progress color='white' category='time' initialData={36} finalData={72}/>
-            <SortItem state='default'>Все</SortItem>
-            <StageProgress color='green' progress={7} isExpired={true}/>
-            <Category type='dev' />
-            <InvoiceProgress sum={220000} type='Закрыт'/>
-            <StageInvoiceAlert>Дизайн</StageInvoiceAlert>
-            <Tooltip type='Планируется'/>
-            <SortDropDown category={"pay_date"} itemType={'default'} />
-            <DropdownItem selected='yes' category='manager' all='no' manager_id={1}>Разработка сайта</DropdownItem>
-            <DropdownContent type='manager'/>
-            <StageItem timeColor='red' profitColor='green' hours={10} percentage={100}>
-                <InvoiceProgress sum={110000} type='Планируется'/>
-            </StageItem>
-            <div>
-                <CardInfoUp title="HyperPC" month="Октябрь" year={2023}/>
-                <CardInfoDown price={139666} remain={139666}>
-                    <StageProgress color='green' isExpired={false} progress={20}/>
-                </CardInfoDown>
-            </div>
-        </main>
-    );
+	return (
+		<main className='flex flex-col items-center justify-center h-screen'>
+			<Header />
+			<Line />
+			<Progress color='white' category='time' initialData={36} finalData={72} />
+			<SortItem state='default'>Все</SortItem>
+			<StageProgress color='green' progress={7} isExpired={true} />
+			<Category type='dev' />
+			<InvoiceProgress sum={220000} type='Закрыт' />
+			<StageInvoiceAlert>Дизайн</StageInvoiceAlert>
+			<Tooltip type='Планируется' />
+			<SortDropDown category={'pay_date'} itemType={'default'} />
+			<DropdownItem selected='yes' category='manager' all='no' manager_id={1}>
+				Разработка сайта
+			</DropdownItem>
+			<DropdownContent type='manager' />
+			<StageItem
+				timeColor='red'
+				profitColor='green'
+				hours={10}
+				percentage={100}
+			>
+				<InvoiceProgress sum={110000} type='Планируется' />
+			</StageItem>
+			<div>
+				<CardInfoUp title='HyperPC' month='Октябрь' year={2023} />
+				<CardInfoDown price={139666} remain={139666}>
+					<StageProgress color='green' isExpired={false} progress={20} />
+				</CardInfoDown>
+			</div>
+		</main>
+	)
 }
--- src/components/invoiceProgress/InvoiceProgress.tsx
+++ src/components/invoiceProgress/InvoiceProgress.tsx
@@ -1,48 +1,53 @@
-import React, {FC} from 'react';
-import {InvoiceProgressProps} from "@/components/invoiceProgress/InvoiceProgress.props";
-import Image from "next/image";
-import classNames from "classnames";
+import React, { FC } from 'react'
+import { InvoiceProgressProps } from '@/components/invoiceProgress/InvoiceProgress.props'
+import Image from 'next/image'
+import classNames from 'classnames'
 
 const getImg = (type: InvoiceProgressProps['type']): string => {
-    if (type === 'Закрыт' || type === 'Транзитный') {
-        return "./img/check-green.svg"
-    }
-    if (type === 'Отправлен') {
-        return "./img/full-blue.svg"
-    }
-    if (type === 'Подготовлен') {
-        return "./img/half-yellow.svg"
-    }
-    if (type === 'Выставить') {
-        return "./img/half-red.svg"
-    }
-    return "./img/full-gray.svg"
+	if (type === 'Закрыт' || type === 'Транзитный') {
+		return './img/check-green.svg'
+	}
+	if (type === 'Отправлен') {
+		return './img/full-blue.svg'
+	}
+	if (type === 'Подготовлен') {
+		return './img/half-yellow.svg'
+	}
+	if (type === 'Выставить') {
+		return './img/half-red.svg'
+	}
+	return './img/full-gray.svg'
 }
 
-const InvoiceProgress: FC<InvoiceProgressProps> = ({sum, type}) => {
-    const imgSrc = getImg(type)
+const InvoiceProgress: FC<InvoiceProgressProps> = ({ sum, type }) => {
+	const imgSrc = getImg(type)
 
-    return (
-        <div className='flex items-center'>
-            <Image
-                src={imgSrc}
-                width={16}
-                height={16}
-                alt="stage-progress"
-                className='mr-1.5'
-            />
-            <span className={classNames(['text-gray opacity-35 text-md font-medium'], {
-                ['text-green !opacity-100']: type === 'Закрыт' || type === 'Транзитный'
-            })}>
-                {new Intl.NumberFormat('ru-RU', {
-                    style: 'currency',
-                    currency: 'RUB',
-                    minimumFractionDigits: 0
-                }).format(sum)}
-            </span>
-            {type === 'Транзитный' && <span className='text-yellow ml-2'>транзит</span>}
-        </div>
-    );
-};
+	return (
+		<div className='flex items-center'>
+			<Image
+				src={imgSrc}
+				width={16}
+				height={16}
+				alt='stage-progress'
+				className='mr-1.5 w-auto h-auto'
+			/>
+			<span
+				className={classNames(['text-gray opacity-35 text-md font-medium'], {
+					['text-green !opacity-100']:
+						type === 'Закрыт' || type === 'Транзитный'
+				})}
+			>
+				{new Intl.NumberFormat('ru-RU', {
+					style: 'currency',
+					currency: 'RUB',
+					minimumFractionDigits: 0
+				}).format(sum)}
+			</span>
+			{type === 'Транзитный' && (
+				<span className='text-yellow ml-2'>транзит</span>
+			)}
+		</div>
+	)
+}
 
 export default InvoiceProgress
